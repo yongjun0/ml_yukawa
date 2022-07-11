31d497bf00cd1d4e9fc21f76c1d46d45cfc3a22c
Yongjun Choi <choiyj@msu.edu>
2022-07-11 10:13:12 -0400

    add grnn routine

--- post-processing/grnn.py
+++ post-processing/grnn.py
@@ -4,6 +4,7 @@ import sys
 import re
 import glob
 import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
 
 def add_values_in_dict(sample_dict, key, list_of_values):
     ''' Append multiple values to a key in 
@@ -13,12 +14,14 @@ def add_values_in_dict(sample_dict, key, list_of_values):
     sample_dict[key].extend(list_of_values)
     return sample_dict
 ###########
-t_kappa = 0.5
-t_gamma = 11
+t_kappa = float(sys.argv[1])
+t_gamma = int(sys.argv[2])
+#t_kappa = 2.2
+#t_gamma = 80 
 ###########
 
 N = 10000
-Npd = 8001
+Npd = 8000
 dt = 0.1
 q_max = 30
 
@@ -73,10 +76,37 @@ for key in kg_dict:
 
 t_sqw /=den_weight
 #data = np.load(path0+'0.5-11.npy')
-data = np.load("../Yukawa/kappa_0.5/gamma_11/data/sqw_avg.npy")
-plt.figure()
-plt.plot(w, t_sqw[0, Npd:], 'r')
-plt.plot(w, 2*data[0, Npd:], 'b')
-plt.xlim(0, 2)
-plt.show()
+#data = np.load("../Yukawa/kappa_2.1/gamma_80/data/sqw_avg.npy")
+data = np.load("data_sample/sqw/"+str(t_kappa)+"-"+str(t_gamma)+".npy")
 
+
+ax = np.zeros(4, object)
+fig, ((ax[0], ax[1]), (ax[2], ax[3])) = plt.subplots(2, 2, sharex = 'col', figsize = (12, 6))
+
+tx = 0.65
+ty = 0.45
+fl = 16
+
+for ij in range(4):
+    nq = (ij)*10
+    ax[ij].plot(w, t_sqw[nq, Npd:], 'r', label='Guess')
+    ax[ij].plot(w, data[nq, Npd:], 'b', label='True')
+
+    label = f"q = {((nq+1)*dq):3.2f}"
+    ax[ij].text(tx, ty, r'$'+label+'$', fontsize = fl, transform=ax[ij].transAxes)
+    ax[ij].set_xlim([0, 2])
+    ax[ij].tick_params(axis='both', which='major', labelsize = fl)
+    ax[ij].legend(loc='upper right', fontsize = fl)
+
+
+ax[2].set_xlabel("$\omega$", fontsize = fl)
+ax[3].set_xlabel("$\omega$", fontsize = fl)
+plt.subplots_adjust(hspace=0)
+plt.suptitle(f"$S(q, \omega), \kappa = {t_kappa}\; \Gamma ={t_gamma}$")
+
+
+#plt.show()
+#pdf=PdfPages(f'figures/{t_kappa}-{t_gamma}.pdf')
+#pdf.savefig(fig)
+#pdf.close()
+plt.savefig(f'figures/{t_kappa}-{t_gamma}.png')
