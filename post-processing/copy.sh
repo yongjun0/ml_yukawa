3b23e35bd35ea3bbb1a60a6ea841dad1d18bb30a
Yongjun Choi <choiyj@msu.edu>
2022-07-15 19:52:28 -0400

    edit copy

---
+++ post-processing/copy.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+kappa="0.5 1 1.5 2 2.5 3"
+g_for_k05="11 183 61"
+g_for_k10="11 14 144 217 234 72"
+g_for_k15="116 22 328"
+g_for_k20="158 26 31 320 449 476"
+g_for_k25="328 60 890"
+g_for_k30="100 1000 1174 1510 503 62"
+
+kappa="2.1 2.2 2.3 2.4 2.5"
+gamma="60 70 80 90 100"
+dest="data_sample/sqw/"
+mkdir -p $dest
+for ik in $kappa; do
+  #if [ "$ik" == "0.5"  ]; then
+  # gamma=$g_for_k05
+  #elif [ "$ik" == "1"  ]; then
+  # gamma=$g_for_k10
+  #elif [ "$ik" == "1.5"  ]; then
+  # gamma=$g_for_k15
+  #elif [ "$ik" == "2"  ]; then
+  # gamma=$g_for_k20
+  #elif [ "$ik" == "2.5"  ]; then
+  # gamma=$g_for_k25
+  #elif [ "$ik" == "3"  ]; then
+  # gamma=$g_for_k30
+  #fi
+  for ig in $gamma; do
+    cp ../Yukawa/kappa_$ik/gamma_$ig/data/sqw_avg.npy ${dest}${ik}-${ig}.npy
+  done
+done
+
+
